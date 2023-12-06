a3dded14170222e1044c11d25ef4585785053487
met4000 <nathsharwor@hotmail.com>
2023-12-06 13:38:55 +0800

    day 6 old

---
+++ 6/part2_old.js
@@ -0,0 +1,20 @@
+(() => {
+  let [times, distances] = document.body.innerText.split("\n").filter(v => v).map(
+    v => [parseInt(v.replace(/^.+?(?=\d)|\s+/g, ""))]
+  );
+
+  let races = times.map((_, i) => ({ time: times[i], record: distances[i] }));
+
+  let product = 1;
+  for (let race of races) {
+    let rangeStart = 0, rangeEnd = race.time;
+    while ((race.time - rangeStart) * rangeStart <= race.record) rangeStart++;
+    while ((race.time - rangeEnd) * rangeEnd <= race.record) rangeEnd--;
+
+    let nWinningValues = rangeEnd - rangeStart + 1;
+
+    product *= nWinningValues;
+  }
+
+  return product;
+})();
