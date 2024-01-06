135e866cc8f40455ad1906018c51d17eb049c731
met4000 <nathsharwor@hotmail.com>
2024-01-06 18:18:34 +0800

    add partial diagonal movement

--- 26/main.js
+++ 26/main.js
@@ -55,7 +55,8 @@ function range(a, b, delta = 1) {
 
 
 
-let str = `８｜♜♞♝♛♚♝♞♜
+//document.body.innerText =
+`８｜♜♞♝♛♚♝♞♜
 ７｜♟︎♟︎♟︎♟︎♟︎♟︎♟︎♟︎
 ６｜　　　　　　　　
 ５｜　　　　　　　　
@@ -68,10 +69,7 @@ let str = `８｜♜♞♝♛♚♝♞♜
 
 [Result "*"]
 
-1. *`;
-
-//document.body.innerText =
-str
+1. *`
 // main program is below:
 
 
@@ -102,47 +100,100 @@ str
   // doesn't know about pawn promotion or en-passant
   // TODO check piece is on board
   // TODO change pawns from $ and % to ^ and _
-  // .replace(/i♙(.*\n.*\n-)/, "$$$$*,%**,♙!$1") // starting rank
-  // .replace(/i♙/, "$$*,%**,♙!")
-  // .replace(/i♗/, "%%%%%%%%****,♗!")
-  // .replace(/i♘/, "<****,>****,♘!")
-  // .replace(/i♖/, "%%%%%%%%****,♖!")
-  // .replace(/i♕/, "$$$$$$$$$$$$$$$$****,%%%%%%%%****,♕!")
-  // .replace(/i♔/, "$$****,%****,♔!")
+  // .replace(/！♙(.*\n.*\nー)/, "||*,_**,♙!$1") // starting rank
+  // .replace(/！♙/, "|*,_**,♙!")
+  // .replace(/！♗/, "%%%%%%%%****,♗!")
+  // .replace(/！♘/, "<****,>****,♘!")
+  // .replace(/！♖/, "%%%%%%%%****,♖!")
+  // .replace(/！♕/, "$$$$$$$$$$$$$$$$****,%%%%%%%%****,♕!")
+  // .replace(/！♔/, "$$****,%****,♔!")
   
   // ! only pawn forward moves and knight moves for now
-  .replace(/！♙(.*\n.*\nー)/, "||*,♙!$1") // starting rank
-  .replace(/！♙/, "|*,♙!")
+  .replace(/！♙(.*\n.*\nー)/, "||*,_**,♙!$1") // starting rank
+  .replace(/！♙/, "|*,_**,♙!")
   .replace(/！♘/, "<****,>****,♘!")
   .replace(/！([♔-♙])/, "$1!")
 
   // uses a @ to mark where the previous check was (and & after moving the check)
 
-  // orthogonal movement ($)
+  // orthogonal movement ($ and |)
   .repeatReplace(v => v
     .replace(/([$|]+)(\*|\*{4}),/, "@`$1$2,")
     // TODO other directions
 
-    .repeatReplace(v => v
-      // increment distance by 1
-      .replace(/([$|])(\*+)/, "$2$1")
+    // increment distance by 1
+    .replace(/([$|])(\*+)/, "$2$1")
 
+    .repeatReplace(v => v
       // if unoccupied, add marker
-      .replace(/([１-８])(｜(?:[♔-♞　]|♟︎)*)(　)((?:[♔-♞　\n１-８｜]|♟︎){10})@((?:(?:[♔-♞　\n１-８｜]|♟︎){10}(?:(?:[♔-♞　\n１-８｜]|♟︎){11})*)?[^\n]*([^\n])!(?:(?:[♔-♞　\n１-８｜]|♟︎){11})*.{21})([ａ-ｈ])(.*Possible Moves:)/s, "$1$2&$4　$5$7$8\n$6 ~$7$1") // up
+      .replace(/([１-８])(｜(?:[♔-♞　]|♟︎)*)(　)((?:[♔-♞　\n１-８｜]|♟︎){10})@(`?(?:(?:[♔-♞　\n１-８｜]|♟︎){10}(?:(?:[♔-♞　\n１-８｜]|♟︎){11})*)?[$|*]+,[^\n]*([^\n])!(?:(?:[♔-♞　\n１-８｜]|♟︎){11})*.{21})([ａ-ｈ])(.*Possible Moves:)/s, "$1$2&$4　$5$7$8\n$6 ~$7$1") // up
       // TODO right, down, left
+
+      // TODO check for wall collision
       
-      // TODO capture only enemy pieces
-      // if not unoccupied, 
-      .replace(/([１-８])(｜(?:[♔-♞　]|♟︎)*)([♚-♞]|♟︎)((?:[♔-♞　\n１-８｜]|♟︎){10})@((?:(?:[♔-♞　\n１-８｜]|♟︎){10}(?:(?:[♔-♞　\n１-８｜]|♟︎){11})*)?[^\n]*([^\n])!(?:(?:[♔-♞　\n１-８｜]|♟︎){11})*.{21})([ａ-ｈ])(.*Possible Moves:)/s, "$1$2&$4　$5$7$8\n$6 ~x$7$1 (-$3)") // up
+      // if not unoccupied and $, remove marker and add move
+      .replace(/([１-８])(｜(?:[♔-♞　]|♟︎)*)([♚-♞]|♟︎)((?:[♔-♞　\n１-８｜]|♟︎){10})@(`?(?:(?:[♔-♞　\n１-８｜]|♟︎){10}(?:(?:[♔-♞　\n１-８｜]|♟︎){11})*)?[$*]+,[^\n]*([^\n])!(?:(?:[♔-♞　\n１-８｜]|♟︎){11})*.{21})([ａ-ｈ])(.*Possible Moves:)/s, "$1$2$3$4　$5$7$8\n$6 ~x$7$1 (-$3)") // up
       // TODO other directions
+      
+      // if occupied and |, remove marker
+      .replace(/@(`?(?:(?:[♔-♞　\n１-８｜]|♟︎){10}(?:(?:[♔-♞　\n１-８｜]|♟︎){11})*)?[|*]+,[^\n]*([^\n])!)/s, "$1") // up
 
       // remove initial preceding/trailing space
       .replace(/　?`　?/, "")
       .replace(/&/, "@")
 
       // TODO deal with final movement in a direction, and changing direction
-      .replace(/@([^$|]+)\*(\**[$|]+),/, "　$1$2,") // remove marker and change to next direction
-      .replace(/([^*])[$|]+,/, "$1") // remove move types with all directions completed
+      .replace(/(^[^@]*)([$|]*)\*(\**)([$|]+)(,)/, "$1$3$2$4$5") // must have hit something; remove any remaining distance and change direction
+      .replace(/@([^$|*]+)\*(\**)([$|]+)(,)/, "　$1$3$2$4") // remove marker and change to next direction
+      .replace(/([^$|*])[$|]+,/, "$1") // remove move types with all directions completed
+
+      // increment distance by 1 (if it is already more than 0)
+      .replace(/([$|])(\*+[$|])/, "$2$1")
+    )
+  )
+
+
+  // diagonal movement (% and _)
+  .repeatReplace(v => v
+    .replace(/([%_]+)(\*|\*{2}),/, "@`$1$2,")
+    // TODO other directions
+
+    // increment distance by 1
+    .replace(/([%_])(\*+)/, "$2$1")
+
+    .repeatReplace(v => v
+      // if unoccupied, move marker and add move
+      .replace(/(　)((?:[♔-♞　\n１-８｜]|♟︎){9})@(`?(?:(?:[♔-♞　\n１-８｜]|♟︎){11}(?:(?:[♔-♞　\n１-８｜]|♟︎){10})*)?[%_]*\*{2}[%_],[^\n]*([^\n])!.*Possible Moves:)/s, "&#$2　$3\n$4 ~～") // up-right
+      .replace(/(　)((?:[♔-♞　\n１-８｜]|♟︎){11})@(`?(?:(?:[♔-♞　\n１-８｜]|♟︎){11}(?:(?:[♔-♞　\n１-８｜]|♟︎){12})*)?[%_]*\*[%_],[^\n]*([^\n])!.*Possible Moves:)/s, "&#$2　$3\n$4 ~～") // up-left
+      // TODO up-right, down-right, down-left
+      
+      // if unoccupied and _, remove added move
+      .replace(/([^,][_]*\*[_],[^\n]*([^\n])!.*Possible Moves:).*~～/s, "$1")
+
+      // if wall, remove marker
+      .replace(/([｜\n])((?:[♔-♞　\n１-８｜]|♟︎){9})@(`?(?:(?:[♔-♞　\n１-８｜]|♟︎){11}(?:(?:[♔-♞　\n１-８｜]|♟︎){10})*)?[%_]*\*{2}[%_],[^\n]*([^\n])!)/s, "$1$2　$3") // up-right
+      .replace(/([｜\n])((?:[♔-♞　\n１-８｜]|♟︎){11})@(`?(?:(?:[♔-♞　\n１-８｜]|♟︎){11}(?:(?:[♔-♞　\n１-８｜]|♟︎){12})*)?[%_]*\*[%_],[^\n]*([^\n])!)/s, "$1$2　$3") // up-left
+      
+      // if occupied, remove marker and add move
+      .replace(/([♚-♞]|♟︎)((?:[♔-♞　\n１-８｜]|♟︎){9})@(`?(?:(?:[♔-♞　\n１-８｜]|♟︎){11}(?:(?:[♔-♞　\n１-８｜]|♟︎){10})*)?[%_]*\*{2}[%_],[^\n]*([^\n])!.*Possible Moves:)/s, "$1#$2　$3\n$4 ~x～ (-$1)") // up-right
+      .replace(/([♚-♞]|♟︎)((?:[♔-♞　\n１-８｜]|♟︎){11})@(`?(?:(?:[♔-♞　\n１-８｜]|♟︎){11}(?:(?:[♔-♞　\n１-８｜]|♟︎){12})*)?[%_]*\*[%_],[^\n]*([^\n])!.*Possible Moves:)/s, "$1#$2　$3\n$4 ~x～ (-$1)") // up-left
+      // TODO other directions
+
+      // remove initial preceding/trailing space
+      .replace(/　?`　?/, "")
+      .replace(/&/, "@")
+
+      // add current marker coords
+      .replace(/([１-８])(｜(?:[♔-♞　]|♟︎)*@?)#((?:(?:(?:[^♔-♞　\n１-８｜♟︎ー]*[♔-♞　\n１-８｜]|♟︎)!?){11})*.{21})([ａ-ｈ])(.*)～/s, "$1$2$3$4$5$4$1") // TODO
+      .replace(/#(.*ー)/s, "$1")
+
+      // TODO deal with final movement in a direction, and changing direction
+      .replace(/(^[^@]*)([%_]*)\*(\**)([%_]+)(,)/, "$1$3$2$4$5") // must have hit something; remove any remaining distance and change direction
+      .replace(/@([^%_*]+)\*(\**)([%_]+)(,)/, "　$1$3$2$4") // remove marker and change to next direction
+      .replace(/([^%_*])[%_]+,/, "$1") // remove move types with all directions completed
+
+      // increment distance by 1 (if it is already more than 0)
+      .replace(/([%_])(\*+[%_])/, "$2$1")
     )
   )
 
@@ -205,6 +256,16 @@ str
 
 .replace(/！/, "")
 
-// TODO sort moves
 
-// TODO select move
+// evaluate the position after each move
+
+// TODO
+
+
+// select move
+
+// TODO sort by evaluation
+
+// choose the top rated move
+.replace(/Possible Moves:\n(. [^\n]*).*/s, "Selected Move: $1")
+.replace(/ |+$/, "")
